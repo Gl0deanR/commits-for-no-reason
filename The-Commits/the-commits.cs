64c813cd5b8005640cb0008a22228a128348bc7a
Raul <raulgldn@gmail.com>
2021-03-10 07:59:38 +0200

    Update the-commits.cs

--- The-Commits/the-commits.cs
+++ The-Commits/the-commits.cs
@@ -5,6 +5,11 @@ namespace Commits
         public static void Main(string[] args)
         {
             Console.WriteLine("Long live The Commit!");
+            Console.WriteLine("WoW!");
+            Console.WriteLine("Born to fight!");
+            Console.WriteLine("Trained to kill!");
+            Console.WriteLine("Ready to die!");
+            Console.WriteLine("But NEVER will!");
         }
     }
 }
